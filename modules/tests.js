85584783906e77a9c0921358320a3435f07b6b4d
RaiCosta <rai.costa@lockton.com>
2024-03-09 08:52:23 -0300

    teste

--- modules/tests.js
+++ modules/tests.js
@@ -1,8 +1,9 @@
 const https = require('https');
+const { port } = require('./modules/configuration');
 
 const testSelf = async() => {
     console.log('sending request to self');
-    https.get('https://10.130.0.20:8062/download?busName=Equatorial&busCountry=Brazil', res => {
+    https.get(`http://127.0.0.1:${port}/download?busName=Equatorial&busCountry=Brazil`, res => {
       let data = [];
       const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
       console.log('Status Code:', res.statusCode);
