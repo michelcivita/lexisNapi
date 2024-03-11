77de6199ada6e951dfba9a384afd58e86719fa92
RaiCosta <rai.costa@lockton.com>
2024-03-11 07:17:03 -0300

    teste

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { port } = require('./modules/configuration');
 const { getDownload } = require('./modules/endpoints');
-const { testSelf } = require('./modules/tests');
+// const { testSelf } = require('./modules/tests');
 
 const app = express();
 app.get('/download', getDownload);
