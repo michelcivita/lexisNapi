d580363c4849c46526be650801f165e7a7dad372
RaiCosta <rai.costa@lockton.com>
2024-03-14 11:03:43 -0300

    teste

--- modules/tests.js
+++ modules/tests.js
@@ -3,7 +3,7 @@ const { port } = require('./configuration');
 
 const testSelf = async() => {
     console.log('sending request to self');
-    http.get(`http://127.0.0.1:${port}/download?busName=Equatorial&busCountry=Brazil`, res => {
+    http.get(`http://localhost:${port}/download?busName=Equatorial&busCountry=Brazil`, res => {
       let data = [];
       const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
       console.log('Status Code:', res.statusCode);
